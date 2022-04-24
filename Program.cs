69a0bac4f28552cd3b61755ff86ad01f26b4de85
Pierre <pierre.github@outlook.fr>
2022-04-24 05:47:01 +0200

    v1

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace HelloWorldConsole
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World! v1");
         }
     }
 }
